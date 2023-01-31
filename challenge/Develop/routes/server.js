e0fd3e2778fcf77b2297c7b3dfb5bc18b5511d63
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-01-30 17:10:53 -0800

    updated server file

---
+++ challenge/Develop/routes/server.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const routesAPI = require( './routes/routesAPI');
+const routesHtml = require('./routes/routesHtml');
+const app = express();
+
+const PORT =process.env.PORT || 3001
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api', routesAPI);
+app.use('/', routesHtml);
+
+app.listen(PORT, ()=> console.log('listening on port: ${PORT}'))
