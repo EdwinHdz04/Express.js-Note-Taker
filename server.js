ae29c5c1d22f6b0e66a726686804ab84a860f895
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-01-30 22:01:10 -0800

    saving untracked file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routesAPI = require( './routes/routesAPI');
+const routesHtml = require('./routes/routesHtml');
+const app = express();
+
+const PORT =process.env.PORT || 3001
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use('/api', routesAPI);
+app.use('/', routesHtml);
+
+app.listen(PORT, () => 
+console.log('listening at http://localhost:${PORT}')
+);
