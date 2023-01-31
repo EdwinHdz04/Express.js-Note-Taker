c6112ee32ed8780aa60b08ce4656435f254e3fdc
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-01-31 13:39:46 -0800

    removed develop folder

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const routesAPI = require( './Develop/routes/routesAPI');
-const routesHtml = require('./Develop/routes/routesHtml');
+const routesAPI = require( './routes/routesAPI');
+const routesHtml = require('./routes/routesHtml');
 const app = express();
 
 const PORT =process.env.PORT || 3001
